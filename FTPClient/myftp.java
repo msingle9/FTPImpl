692016b0c236ffef83bc35a73c0709c43d948f3f
Christine McGee <52633421+CBCelest@users.noreply.github.com>
2020-01-25 23:08:34 -0500

    Fix bugs and add comments.

--- FTPClient/myftp.java
+++ FTPClient/myftp.java
@@ -9,46 +9,62 @@ import java.util.concurrent.TimeUnit;
 
 
 /**
- * @author Christine McGee
+ * A simple FTP Client program that takes two command line parameters.
+ * The first is the machine name where the server resides and the 
+ * second is the port number the server is listening on. 
+ * @author Christine McGee, Andrew Heywood, Matthew Singletary
  *
  */
 public class myftp {
 
+	// Variable Declarations
 	private static Socket socket = null;
 	private static String hostName = null;
 	private static int portNumber = 0;
 	private static Future clientProgramFuture = null;
 
 	/**
+	 * Begins execution of Client program by verifying correct user
+	 * command line input and creating the socket connection to the 
+	 * server via the hostName and portNumber.
 	 * @param args args[0] as host name and args[1] as port number
 	 */
 	public static void main(String[] args) throws Exception {
         
+		// Create thread pool of one for client to run in
 		ExecutorService threadPoolClient = Executors.newFixedThreadPool(1);
 		
-
+		// Verify user entered host name and port number
+		// if not inform them and exit.
 		if (args.length != 2) {
-            	System.err.println("Pass the  machine name where the server resides and the port number");
-            	return;            
-        	}
+            System.err.println("Pass the machine name where the server resides and the port number");
+            System.err.println("Usage: myftp HOST NAME PORTNUMBER");
+            System.exit(-1);;            
+        }
         		
-		String hostName = args[0];
+		// Assign first command line argument to hostName		
+		hostName = args[0];
 		
-		try {	        
+		// Try to parse int from string of second command line
+		// argument and assign to portNumber.
+		// If error inform user and exit.
+		try {
+	        
 			portNumber = Integer.parseInt(args[1]);
-	    	} 		
+	    } 		
 		catch (NumberFormatException e) {
 	        
-			System.err.println("Command line argument" + args[0] + " must be a port integer."); //FIX
+			System.err.println("Command line argument " + args[1] + " must be a valid port number.");
 	        System.exit(-1);
 	    }
-		
-		
-		        
-        try {
+
+		// Try creating a stream socket and connect it to the specified port number
+		// on the named host. Create new Runnable FTPClientWorker and pass to 
+		// thread executor. Upon thread completion (user entered quit) return to here and proceed
+		//  with thread and program shutdown. Catch Possible errors and inform user.
+		try {
 
         	socket = new Socket(hostName, portNumber);
-        	//socket = new Socket("localhost", 8015);
 
         	Runnable clientProgram = new FTPClientWorker(socket);
        		
@@ -67,7 +83,7 @@ public class myftp {
        		           System.err.println("Pool did not terminate");
        		     }
        		   } catch (InterruptedException ie) {
-       		     // (Re-)Cancel if current thread also interrupted
+       		     // Re-Cancel if current thread also interrupted
        			threadPoolClient.shutdownNow();
        		     // Preserve interrupt status
        		     Thread.currentThread().interrupt();
@@ -78,14 +94,11 @@ public class myftp {
         }
         catch (IOException e) {
             System.err.println("Input Output Exception: " + e.getMessage());
+	        System.exit(-1);
         }
         
         if(threadPoolClient.isShutdown() || threadPoolClient.isTerminated()) {
         	System.out.println("Connection closed");
         }
 	}
-	
-
-		
-
 }
